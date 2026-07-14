f022418239c3ca366096028fd8997b72ba3632f0
Dmitriy <edv.1979.dmitriy@gmail.com>
2026-07-14 22:33:08 +0300

    Solved problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        int startBalance;
+        int summaChange;
+        int bonus;
+        int endBalance;
 
+        startBalance = 888;
+        summaChange = 2010;
+
+        if (summaChange > 1000) {
+            bonus = summaChange / 100;
+            endBalance = startBalance + summaChange + bonus;
+            System.out.println(" Ваш бонус за пополнение счёта на 1000 рублей и более: " + bonus + " руб.");
+            System.out.println(" Ваш баланс: " + endBalance + " руб.");
+        } else {
+            endBalance = startBalance + summaChange;
+            System.out.println(" Ваш бонус за пополнение счёта на 1000 рублей и более: 0 руб.");
+            System.out.println(" Ваш баланс: " + endBalance + " руб.");
+        }
     }
 }
